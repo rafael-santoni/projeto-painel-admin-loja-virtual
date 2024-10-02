fb78cdd429d690e3ab84a24f451cc1097e0efb31
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-10-02 13:12:30 -0300

    Aula #07: Adicionado o número total de Clientes cadastrados no site e o total de Usuários Online no dashboard da página inicial do painel admin

--- App/Controllers/Admin/PainelController.php
+++ App/Controllers/Admin/PainelController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\Site\UserModel;
+use App\Models\Site\UsersOnlineModel;
 use App\Repositories\Admin\PedidosRepository;
 
 class PainelController extends BaseController {
@@ -13,10 +15,18 @@ class PainelController extends BaseController {
         $totalVendas = $pedidosRepository->totalVendas();
         $totalVendasMes = $pedidosRepository->totalVendasMes();
 
+        $userModel = new UserModel;
+        $usersEncontrados = $userModel->fetchAll();
+
+        $usersOnlineModel = new UsersOnlineModel;
+        $totalUsersOnline = $usersOnlineModel->fetchAll();
+
         $dados = [
             'titulo' => 'Loja Virtual - RS-Dev | Painel Administrativo',
             'totalVendas' => $totalVendas,
-            'totalVendasMes' => $totalVendasMes
+            'totalVendasMes' => $totalVendasMes,
+            'clientes' => count($usersEncontrados),
+            'totalUsuariosOnline' => count($totalUsersOnline),
         ];
 
         $template = $this->twig->load('admin_home.html');
