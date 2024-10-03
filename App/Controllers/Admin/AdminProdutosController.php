5f1b83d7cb9e6d9471d9186e2e922b82d460776c
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-10-03 12:05:51 -0300

    Aula #18: Criada a View no Painel Administrativo contendo a Listagem de Produtos da loja virtual

---
+++ App/Controllers/Admin/AdminProdutosController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+use App\Models\Admin\ProdutoModel;
+
+class AdminProdutosController extends BaseController {
+
+    public function index(){
+
+        $produtosModel = new ProdutoModel;
+        $produtosEncontrados = $produtosModel->fetchAll();
+
+        $dados = [
+            'titulo' => 'Loja Virtual - RS-Dev | Painel Administrativo | Lista de Produtos',
+            'produtos' => $produtosEncontrados
+        ];
+
+        $template = $this->twig->load('admin_listar_produtos.html');
+        echo $template->render($dados);
+
+    }
+
+}
