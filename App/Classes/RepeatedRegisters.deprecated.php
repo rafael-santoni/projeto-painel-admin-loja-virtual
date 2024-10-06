5577fe12dfa1ed1e40c251b98eccdba607615b94
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-10-06 13:55:09 -0300

    Aula #48: Refeito o código da classe RepeatedRegisters para melhorar a validação de registros repetidos em uma tabela

---
+++ App/Classes/RepeatedRegisters.deprecated.php
@@ -0,0 +1,24 @@
+<?php
+/*
+    --************************--
+    ***** Deprecated Class *****
+    --************************--
+
+namespace App\Classes;
+
+use App\Interfaces\InterfaceRepeatedRegisters;
+
+class RepeatedRegisters {
+
+    private $repeatedRegisters;
+
+    public function __construct(InterfaceRepeatedRegisters $repeat){
+        $this->repeatedRegisters = $repeat;
+    }
+
+    public function validate($rules){
+        $this->repeatedRegisters->validate ($rules);
+    }
+
+}
+*/
--- App/Classes/RepeatedRegisters.php
+++ App/Classes/RepeatedRegisters.php
@@ -2,18 +2,45 @@
 
 namespace App\Classes;
 
-use App\Interfaces\InterfaceRepeatedRegisters;
-
 class RepeatedRegisters {
 
-    private $repeatedRegisters;
+    private $rules;
+    private $model;
+    private $field;
 
-    public function __construct(InterfaceRepeatedRegisters $repeat){
-        $this->repeatedRegisters = $repeat;
+    public function __construct($rules){
+        $this->rules = $rules;
     }
 
-    public function validate($rules){
-        $this->repeatedRegisters->validate ($rules);
+    private function getModelAndField(){
+
+        foreach ($this->rules as $field => $rule) {
+
+            if(substr_count($rule, ':') > 0) {
+
+                list($validate, $model) = explode(':', $rule);
+                $this->model = $model;
+                $this->field = $field;
+
+            }
+
+        }
+
+        return [
+            'model' => new $this->model,
+            'field' => $this->field
+        ];
+
+    }
+
+    public function validate(){
+
+        $modelAndField = $this->getModelAndField();
+
+        if($modelAndField['model']->find($modelAndField['field'], $_POST[$modelAndField['field']])) {
+            ErrorsValidate::add($modelAndField['field'], 'Já existe um registro cadastrado com esse valor');
+        }
+
     }
 
 }
